7911f6d4be578a92d2b04c5a3d050946b013aed9
keyusuf <mrkenanyusuf@gmail.com>
2023-12-05 10:51:43 -0500

    Your commit message

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+# instantiate the app
+from flask import Flask
+from flask_restx import Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+# set config
+app.config.from_object('src.config.DevelopmentConfig')
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+api.add_resource(Ping, '/ping')

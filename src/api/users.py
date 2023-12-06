1b2bea9c308857a8285037d7c038c95e73272c31
keyusuf <mrkenanyusuf@gmail.com>
2023-12-05 20:31:07 -0500

    Final Commit

--- src/api/users.py
+++ src/api/users.py
@@ -16,29 +16,27 @@ user_model = api.model('User', {
 })
 
 class UsersList(Resource):
-    @api.expect(user_model, validate=True)  # Expecting user_model for POST request
+    @api.expect(user_model, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get('username')
         email = post_data.get('email')
         response_object = {}
 
-        # Check if user already exists
         user = User.query.filter_by(email=email).first()
         if user:
             response_object['message'] = 'Sorry. That email already exists.'
             return response_object, 400
 
-        # Add new user to the database
         new_user = User(username=username, email=email)
         db.session.add(new_user)
         db.session.commit()
         response_object['message'] = f'{email} was added!'
         return response_object, 201
 
-    @api.marshal_with(user_model, as_list=True)  # Serialize list of users
+    @api.marshal_with(user_model, as_list=True)
     def get(self):
-        return User.query.all(), 200  # Fetch and return all users
+        return User.query.all(), 200
 
 class Users(Resource):
     @api.marshal_with(user_model)
@@ -48,6 +46,28 @@ class Users(Resource):
             api.abort(404, f"User {user_id} does not exist")
         return user
 
-api.add_resource(UsersList, '/users')  # For listing and creating users
-api.add_resource(Users, '/users/<int:user_id>')  # For retrieving a single user
+    @api.expect(user_model, validate=True)
+    def put(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
 
+        post_data = request.get_json()
+        user.username = post_data.get('username', user.username)
+        user.email = post_data.get('email', user.email)
+        db.session.commit()
+
+        return {"message": f"User {user_id} was updated."}, 200
+
+    def delete(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+
+        db.session.delete(user)
+        db.session.commit()
+
+        return {"message": f"User {user_id} was deleted."}, 200
+
+api.add_resource(UsersList, '/users')
+api.add_resource(Users, '/users/<int:user_id>')
